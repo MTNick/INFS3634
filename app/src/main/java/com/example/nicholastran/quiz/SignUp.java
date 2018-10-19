e7cfd11885546957b6f87237c62714cc7c936e1d
Nicholas Tran <nicholas.tran1506@gmail.com>
2018-10-19 16:56:50 +1100

    Deleted settings. Changed launch activity to sign up page

--- app/src/main/java/com/example/nicholastran/quiz/SignUp.java
+++ app/src/main/java/com/example/nicholastran/quiz/SignUp.java
@@ -1,7 +1,11 @@
 package com.example.nicholastran.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.nicholastran.quiz.Controllers.MainActivity;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void goToLogIn(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
