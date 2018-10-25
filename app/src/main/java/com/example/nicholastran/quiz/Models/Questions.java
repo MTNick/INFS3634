9df91b25f6483f4f6d27f9cfcf8c80d182c021ac
Syam Iyer <iyersyam@gmail.com>
2018-10-25 19:44:55 +1100

    Added the UI for coding challenges

--- app/src/main/java/com/example/nicholastran/quiz/Models/Questions.java
+++ app/src/main/java/com/example/nicholastran/quiz/Models/Questions.java
@@ -7,6 +7,11 @@ public class Questions {
     public static ArrayList<Questions> questions = new ArrayList<>();
     public static ArrayList<Boolean> userProgress = new ArrayList<>();
     public static double currentProgress;
+    public static String[] challenges = {
+            "Print all numbers from 1 to 5",
+            "Write a sorting function to sort 0, -1, 5, 2, 6 in ascending order",
+            "Make a class called Car. Instantiate a Car object called Mazda. Print the Mazda's color"
+    };
 
     private String question;
     private String answer;
