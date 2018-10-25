410b39c1067549b8ae5b8b9e739733e03335e3a0
Syam Iyer <iyersyam@gmail.com>
2018-10-25 15:26:15 +1100

    Added support to run and compile User's Java code

--- app/src/main/java/com/example/nicholastran/quiz/Controllers/SignUp.java
+++ app/src/main/java/com/example/nicholastran/quiz/Controllers/SignUp.java
@@ -1,23 +1,31 @@
 package com.example.nicholastran.quiz.Controllers;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bigkoo.svprogresshud.SVProgressHUD;
-import com.example.nicholastran.quiz.Controllers.MainActivity;
 import com.example.nicholastran.quiz.Models.FirebaseHelper;
 import com.example.nicholastran.quiz.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
 import com.kaopiz.kprogresshud.KProgressHUD;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SignUp extends AppCompatActivity {
 
@@ -26,6 +34,7 @@ public class SignUp extends AppCompatActivity {
     EditText password;
     EditText confirmPassword;
     FirebaseAuth mAuth;
+    JsonObjectRequest jsonOR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,10 @@ public class SignUp extends AppCompatActivity {
         email = findViewById(R.id.editTextEmail);
         password = findViewById(R.id.editTextPassword);
         confirmPassword = findViewById(R.id.editTextConfirm);
+
+        // Compile the user's code
+        String userInput = "System.out.println(\"Hello world!\");";
+        firstServiceCall(userInput);
     }
 
     @Override
@@ -86,5 +99,62 @@ public class SignUp extends AppCompatActivity {
             new SVProgressHUD(this).showErrorWithStatus("Passwords must match and not be empty");
         }
 
+    }
+
+    public void firstServiceCall(String userInput) {
+        String code = "class Main {" +
+                "public static void main(String[] args) {" +
+                userInput +
+                "}}";
+        String query = Uri.encode(code);
+        String url = "https://api.judge0.com/submissions/?base64_encoded=false&wait=false&language_id=26&source_code=" + query;
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String token = response.getString("token");
+                            final String url2 = "https://api.judge0.com/submissions/" + token;
+                            final Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    secondServiceCall(url2);
+                                }
+                            }, 5000);
+
+                        }
+                        catch (JSONException e) {
+                            System.out.println("ERROR: " + e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        Volley.newRequestQueue(getApplicationContext()).add(jsonObjReq);
+    }
+    public void secondServiceCall(String url) {
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("RESPONSE: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        Volley.newRequestQueue(getApplicationContext()).add(jsonObjReq);
     }
 }
